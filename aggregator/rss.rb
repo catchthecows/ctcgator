830a719b775ae0cdfebd822614193eb8301fd725
Scott Lund <scott@catchthecows.com>
2013-04-10 20:56:40 -0500

    roughing in structure

---
+++ aggregator/rss.rb
@@ -0,0 +1,12 @@
+require 'rss'
+require 'open-uri'
+
+# url = 'http://www.ruby-lang.org/en/feeds/news.rss'
+url = 'http://feeds.feedburner.com/colossal'
+open(url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      puts "Title: #{feed.channel.title}"
+      feed.items.each do |item|
+        puts item
+      end
+end
