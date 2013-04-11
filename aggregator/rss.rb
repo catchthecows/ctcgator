937f4acd5fc74e96167e0dd0dee23b87fff676cf
Scott Lund <scott@catchthecows.com>
2013-04-10 21:05:53 -0500

    test

--- aggregator/rss.rb
+++ aggregator/rss.rb
@@ -1,12 +1,14 @@
 require 'rss'
 require 'open-uri'
 
-# url = 'http://www.ruby-lang.org/en/feeds/news.rss'
+#url = 'http://www.ruby-lang.org/en/feeds/news.rss'
 url = 'http://feeds.feedburner.com/colossal'
 open(url) do |rss|
       feed = RSS::Parser.parse(rss)
       puts "Title: #{feed.channel.title}"
       feed.items.each do |item|
-        puts item
+        puts item.title
+        puts item.pubDate
+        #puts item
       end
 end
