5f06ddf420b4334dde75b44b3ac51e308dacdfc6
Scott Lund <scott@catchthecows.com>
2013-05-18 22:25:02 -0500

    import google feeds from api/0/subscriptions/list

--- import/importgoogle.rb
+++ import/importgoogle.rb
@@ -6,7 +6,7 @@ require_relative '../models/init'
 def show(msg)
 end
 
-def import(filename)
+def import_opml(filename)
     source = XML::Parser.file(filename)
     document = source.parse
     content = document.find('//outline').find
@@ -33,5 +33,67 @@ def import(filename)
     end
 end
 
-import "#{ENV['DATA_DIR']}/subscriptions.xml"
+def parse_categories(node)
+    cats = Array.new
+    n = node.first
+    while (n) do 
+        if n.name=='object' then
+            n1 = n.first
+            while (n1) do
+                if n1.name=='string' && n1['name']=='label' then 
+                    category = n1.content
+                    category.strip!
+                    cats.push category
+                end
+                n1 = n1.next
+            end
+        end
+        n = n.next
+    end
+    puts "category #{cats.join(',')}"
+end
+
+def parse_object(node) 
+    puts "parse object"
+    n = node.first
+    while (n) do 
+        if n.name=='string' && n['name']=='title' then 
+            title = n.content
+            title.strip!
+            puts "title #{title}"
+        end
+        if n.name=='string' && n['name']=='id' then 
+            url = n.content
+            url.strip!
+            puts "url #{url}"
+        end
+        if n.name=='list' && n['name']=='categories' then
+            parse_categories n
+        end
+        n = n.next
+    end
+end
+
+def import_subscriptions(filename)
+    source = XML::Parser.file(filename)
+    document = source.parse
+    content = document.find('//list') #.find
+    
+    content.each do | l |
+        if (l['name'] == 'subscriptions') then
+            n = l.first
+            while (n) do
+                if !n.text? then
+                   if (n.name == 'object') then
+                       parse_object n
+                   end 
+                end
+                n = n.next
+            end
+        end
+    end
+end
+
+#import_opml "#{ENV['DATA_DIR']}/subscriptions.xml"
+import_subscriptions "#{ENV['DATA_DIR']}/list.txt.full"
 
